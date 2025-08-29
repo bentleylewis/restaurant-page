af57fa3464a553d13f6607d43709b87e8e0fd5f9
bentleylewis <bbentleylewis@gmail.com>
2025-08-28 21:29:58 -0500

    intial page load (home) & button functionality

--- src/home.js
+++ src/home.js
@@ -1,8 +1,26 @@
 function createRestaurantHomePage() {
     const contentDiv = document.getElementById("content");
-    const headerDiv = document.createElement("div");
-    headerDiv.textContent = "this is where to header text will go!!";
-    contentDiv.appendChild(headerDiv);
+
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("content-card");
+
+    const introDiv = document.createElement("div");
+    introDiv.textContent = "Welcome To Kenzie's Kitchen";
+    introDiv.id = "intro-title";
+
+    const subIntroDiv = document.createElement("div");
+    subIntroDiv.innerHTML = "Cooked to Perfection.<br> Every Time.";
+    subIntroDiv.id = "intro-subtitle";
+
+    const introDescriptionDiv = document.createElement("div");
+    introDescriptionDiv.textContent = "Kenzie's Kitchen showcases homemade dishes crafted with love and creativity. From comforting classics to inventive creations, every meal is made fresh and served with care.";
+    introDescriptionDiv.id = "intro-description";
+
+    cardDiv.appendChild(introDiv);
+    cardDiv.appendChild(subIntroDiv);
+    cardDiv.appendChild(introDescriptionDiv);
+
+    contentDiv.appendChild(cardDiv);
 }
 
 export default createRestaurantHomePage;
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,7 +1,35 @@
 import createRestaurantHomePage from "./home";
 
 function initialPageLoad () {
+
+    const buttons = document.querySelectorAll("button");
+
     createRestaurantHomePage();
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const text = btn.textContent;
+            const content = document.getElementById('content');
+
+            buttons.forEach(btn=>btn.style.color = '');
+            
+            btn.style.color = '#ffb366';
+            content.innerHTML = '';
+
+            if(text === "Kenzie's Kitchen" || text === "Home") {
+                createRestaurantHomePage();
+            }
+            
+
+            if(text === "Menu") {
+                document.getElementById('content').innerHTML = '';
+            }
+
+            if(text === "Contact Us") {
+                document.getElementById('content').innerHTML = '';
+            }
+        })
+    })
 }
 
 export default initialPageLoad;
